ebce2c732a37c8a8b0312ce41c2b7f7f0d57c425
ivanburda1986 <burda.ivan@gmail.com>
2020-05-14 01:29:11 +0200

    finished successfully an experiment with loading transitions on html elements with js when scrolling

---
+++ dist/js/app.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', () => {
+  let ivan = document.getElementById("ivan");
+  let scrollable = document.documentElement.scrollHeight - window.innerHeight;
+  let scrollY = window.scrollY;
+  let position = ivan.getBoundingClientRect();
+
+  console.log(position);
+  console.log(scrollY);
+  console.log(scrollable);
+
+  if (scrollY > 580) {
+    ivan.classList.add("anim2");
+  }
+})
