aae0d7ecae387d8e554ed73c819c8c2bc8d7757c
ivanburda1986 <burda.ivan@gmail.com>
2020-05-17 22:32:16 +0200

    outline steps for creating the overlay gallery

---
+++ dist/js/gallery.js
@@ -0,0 +1,13 @@
+/*
+-an array with images to show
+-dynamically create a UI element for each image in the array
+-clicking any gallery image will trigger a site overlay and the image will be displayed in the overlay
+-clicking the forward button will show the next image
+-clicking the back button will show the previous image
+-clicking the cross will close the overlay
+
+Static UI:
+-an overlay gallery set to hidden
+-back/forward buttons: always active, loop navigation
+-close button to close the gallery overlay
+*/
