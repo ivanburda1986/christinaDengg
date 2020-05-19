95b1b520fb030e546656132baa302dfbab5bfdb0
ivanburda1986 <burda.ivan@gmail.com>
2020-05-20 01:18:23 +0200

    implement the show-next-photo feature

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,8 +4,11 @@ const gallery = new galleryUI();
 //UI Selectors
 const galleryHideButton = document.getElementById("gallery-hide-btn");
 const galleryGrid = document.getElementById("gallery-grid");
+const nextButton = document.getElementById("gallery-next-btn");
+const previousButton = document.getElementById("gallery-previous-btn");
 
-
+//Variables
+const totalPhotos = photos.length;
 
 
 //Populate the gallery overview
@@ -13,9 +16,19 @@ gallery.listPhotos(photos);
 
 //Show the gallery overlay
 galleryGrid.addEventListener("click", (e) => {
-  console.log(e.target)
   gallery.showGalleryOverlay();
+  gallery.showFullPhoto(e.target.id);
 });
+
+//Show the next full photo
+nextButton.addEventListener("click", () => {
+  const currentFullPhoto = document.querySelector(".gallery-full-photo").id;
+  const currentFullPhotoNumber = photos.indexOf(currentFullPhoto);
+  gallery.showNextFullPhoto(currentFullPhotoNumber, totalPhotos);
+});
+
+//Show the previous full photo
+
 
 //Hide the gallery overlay
 galleryHideButton.addEventListener("click", (e) => {
--- dist/js/gallery-ui.js
+++ dist/js/gallery-ui.js
@@ -2,6 +2,7 @@ class galleryUI {
   constructor() {
     this.galleryOverview = document.getElementById("gallery-grid");
     this.galleryOverlay = document.getElementById("gallery-overlay");
+    this.fullPhotoContainer = document.getElementById("gallery-full-photo-container");
   }
 
   //Display in the UI an overview of available photos
@@ -10,7 +11,7 @@ class galleryUI {
     photos.forEach(photo => {
       galleryOverviewPhotos += `
       <div class="gallery-thumbnail" value="${photos.indexOf(photo)}">
-      <img src="/dist/img/gallery/${photo}" alt="${photo}"
+      <img src="/dist/img/gallery/${photo}" alt="${photo}" id="${photo}"
       />
       </div>
       `
@@ -22,6 +23,26 @@ class galleryUI {
     this.galleryOverlay.classList.add("show");
     this.galleryOverlay.classList.remove("hide");
   }
+
+  showFullPhoto(fullPhotoName) {
+    this.fullPhotoContainer.innerHTML = `<img src="/dist/img/gallery/${fullPhotoName}" alt="${fullPhotoName}" class="gallery-full-photo" id="${fullPhotoName}"/>`;
+  }
+
+  showNextFullPhoto(currentFullPhotoNumber, totalPhotos) {
+    let nextPhotoNumber;
+    if (currentFullPhotoNumber === totalPhotos - 1) {
+      nextPhotoNumber = 1;
+    } else {
+      nextPhotoNumber = currentFullPhotoNumber + 2;
+    }
+    console.log(nextPhotoNumber);
+    this.fullPhotoContainer.innerHTML = `<img src="/dist/img/gallery/photo${nextPhotoNumber}.jpg" alt="photo${nextPhotoNumber}.jpg" class="gallery-full-photo" id="photo${nextPhotoNumber}.jpg"/>`;
+  }
+
+  showNextPreviousPhoto(currentPhotoNumber, totalPhotos) {
+
+  }
+
 
   hideGalleryOverlay() {
     this.galleryOverlay.classList.add("hide");
