6b29a47f2d59e2a447d4ad7bde1bbd431bae7c34
ivanburda1986 <burda.ivan@gmail.com>
2020-05-23 22:30:25 +0200

    implementing a meaningful animate-on-scroll behavior

--- dist/js/animations.js
+++ dist/js/animations.js
@@ -1,32 +1,43 @@
 window.addEventListener('scroll', () => {
-  let christinaImage = document.getElementById("christina-image");
-  let christinaText = document.getElementById("christina-text");
-  let gallerySection = document.getElementById("section-gallery");
   let performancesHeadline = document.getElementById("performances-headline");
-  //let scrollable = document.documentElement.scrollHeight - window.innerHeight;
-  let scrollY = window.scrollY;
-  let christinaPosition = christinaImage.getBoundingClientRect();
-  let christinaTriggerPosition = (christinaPosition.bottom - christinaPosition.height);
 
-  let galleryPosition = gallerySection.getBoundingClientRect();
-  let galleryTriggerPosition = (galleryPosition.top);
+  let windowInnerHeight = window.innerHeight;
+  let yAxisScrolled = document.documentElement.scrollTop;
 
-  //console.log(galleryPosition);
-  console.log(`scrollY: ${scrollY}`);
-  //console.log(scrollable);
-  //console.log(`Position top: ${galleryPosition.top}`);
-  console.log(`Trigger position: ${galleryTriggerPosition}`);
+  let performancesHeadlinePositionFull = performancesHeadline.getBoundingClientRect();
+  let performancesHeadlinePositionTop = performancesHeadline.getBoundingClientRect().top;
 
-  if (scrollY > christinaTriggerPosition) {
-    christinaImage.classList.add("anim-opacity-endState");
-    christinaImage.classList.add("anim-slideDown-endState");
-    christinaText.classList.add("anim-opacity-endState");
-    christinaText.classList.add("anim-slideLeft-endState");
+  let performancesTriggerPosition = windowInnerHeight / 1.5;
 
-  }
-
-  if (scrollY > galleryTriggerPosition) {
-    console.log('now');
+  if (performancesHeadlinePositionTop < performancesTriggerPosition) {
+    console.log('------------------now---------------');
     performancesHeadline.classList.add("anim-opacity-endState");
   }
+
+  console.log(`1)Window inner height: ${windowInnerHeight}`);
+  console.log(`2)Performance headline position top: ${performancesHeadlinePositionTop}`);
+  console.log(`3)Trigger position: ${performancesTriggerPosition}`);
+  //console.log(`4)Already scrolled on the y-axis: ${yAxisScrolled}`);
 })
+
+
+
+
+// let christinaImage = document.getElementById("christina-image");
+// let christinaText = document.getElementById("christina-text");
+
+// let christinaPosition = christinaImage.getBoundingClientRect();
+// let christinaTriggerPosition = (christinaPosition.bottom - christinaPosition.height);
+
+// if (scrollY > christinaTriggerPosition) {
+//   christinaImage.classList.add("anim-opacity-endState");
+//   christinaImage.classList.add("anim-slideDown-endState");
+//   christinaText.classList.add("anim-opacity-endState");
+//   christinaText.classList.add("anim-slideLeft-endState");
+
+// }
+
+// 1)Window height: 3577
+// 2)Scrolled: y-axis: 2009
+// 3)Performance headline top position: 353.5
+// 4)Trigger position: 827.75
