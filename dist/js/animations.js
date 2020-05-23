7638f8648c3501e848ffdaf63d31af77397424b3
ivanburda1986 <burda.ivan@gmail.com>
2020-05-23 15:39:44 +0200

    working on the gallery trigger animations

--- dist/js/animations.js
+++ dist/js/animations.js
@@ -1,22 +1,32 @@
 window.addEventListener('scroll', () => {
   let christinaImage = document.getElementById("christina-image");
   let christinaText = document.getElementById("christina-text");
+  let gallerySection = document.getElementById("section-gallery");
+  let performancesHeadline = document.getElementById("performances-headline");
   //let scrollable = document.documentElement.scrollHeight - window.innerHeight;
   let scrollY = window.scrollY;
-  let position = christinaImage.getBoundingClientRect();
-  let triggerPosition = (position.bottom - position.height);
+  let christinaPosition = christinaImage.getBoundingClientRect();
+  let christinaTriggerPosition = (christinaPosition.bottom - christinaPosition.height);
 
-  //console.log(position);
-  //console.log(`scrollY: ${scrollY}`);
+  let galleryPosition = gallerySection.getBoundingClientRect();
+  let galleryTriggerPosition = (galleryPosition.top);
+
+  //console.log(galleryPosition);
+  console.log(`scrollY: ${scrollY}`);
   //console.log(scrollable);
-  //console.log(`Position top: ${position.top}`);
-  //console.log(`Trigger position: ${triggerPosition}`);
+  //console.log(`Position top: ${galleryPosition.top}`);
+  console.log(`Trigger position: ${galleryTriggerPosition}`);
 
-  if (scrollY > triggerPosition) {
+  if (scrollY > christinaTriggerPosition) {
     christinaImage.classList.add("anim-opacity-endState");
     christinaImage.classList.add("anim-slideDown-endState");
     christinaText.classList.add("anim-opacity-endState");
     christinaText.classList.add("anim-slideLeft-endState");
 
+  }
+
+  if (scrollY > galleryTriggerPosition) {
+    console.log('now');
+    performancesHeadline.classList.add("anim-opacity-endState");
   }
 })
