ce58a664709330a6e2f2d140f8cbee09601ecbc7
ivanburda1986 <burda.ivan@gmail.com>
2020-05-28 00:00:30 +0200

    joined the js configs into a single file

--- dist/js/config.js
+++ dist/js/config.js
@@ -1,3 +1,7 @@
+//Video config
 const performanceVideo1 = `<iframe width="auto" height="auto" src="https://www.youtube.com/embed/F1z6ubHowgA?autoplay=1&rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 const performanceVideo2 = `<iframe width="auto" height="auto" src="https://www.youtube.com/embed/UjxS9ciNlII?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 const performanceVideo3 = `<iframe width="auto" height="auto" src="https://www.youtube.com/embed/qPNiIeKMHyg?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+//Gallery config
+const photos = ['photo1.jpg', 'photo2.jpg', 'photo3.jpg', 'photo4.jpg', 'photo5.jpg', 'photo6.jpg', 'photo7.jpg', 'photo8.jpg', 'photo9.jpg', 'photo10.jpg'];
