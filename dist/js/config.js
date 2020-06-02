4d20fc2bbc92fc90325e5f340c1c8beb4d64b412
ivanburda1986 <burda.ivan@gmail.com>
2020-06-02 23:36:20 +0200

    added final photo content

--- dist/js/config.js
+++ dist/js/config.js
@@ -1,5 +1,5 @@
 //Video config
-const performanceVideo1 = `<iframe width="auto" height="auto" src="https://www.youtube.com/embed/F1z6ubHowgA?autoplay=1&rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+const performanceVideo1 = `<iframe src="https://player.vimeo.com/video/425277195" width="640" height="1138" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>`;
 const performanceVideo2 = `<iframe width="auto" height="auto" src="https://www.youtube.com/embed/UjxS9ciNlII?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 const performanceVideo3 = `<iframe width="auto" height="auto" src="https://www.youtube.com/embed/qPNiIeKMHyg?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 
