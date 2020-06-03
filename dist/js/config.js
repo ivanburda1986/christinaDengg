99acaf67e9fcecda6fba114f7e1d04b7bfe557de
ivanburda1986 <burda.ivan@gmail.com>
2020-06-03 23:37:15 +0200

    added the real content

--- dist/js/config.js
+++ dist/js/config.js
@@ -1,7 +1,7 @@
 //Video config
-const performanceVideo1 = `<iframe src="https://player.vimeo.com/video/425277195" width="640" height="1138" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>`;
-const performanceVideo2 = `<iframe width="auto" height="auto" src="https://www.youtube.com/embed/UjxS9ciNlII?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-const performanceVideo3 = `<iframe width="auto" height="auto" src="https://www.youtube.com/embed/qPNiIeKMHyg?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+const performanceVideo1 = `<iframe width="560" height="315" src="https://www.youtube.com/embed/msgBOBrfi8c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+const performanceVideo2 = `<iframe width="560" height="315" src="https://www.youtube.com/embed/y28JPozFNko" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+const performanceVideo3 = `<iframe width="560" height="315" src="https://www.youtube.com/embed/3zWdQxuqLeg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 
 //Gallery config
 const photos = ['photo1.jpg', 'photo2.jpg', 'photo3.jpg', 'photo4.jpg', 'photo5.jpg', 'photo6.jpg', 'photo7.jpg', 'photo8.jpg', 'photo9.jpg', 'photo10.jpg'];
